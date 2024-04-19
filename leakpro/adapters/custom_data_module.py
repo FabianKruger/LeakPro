16c5a6155109831817882020af1ff3986cd46ae9
Fabian Krüger <132886103+FabianKruger@users.noreply.github.com>
2024-04-19 17:42:36 +0200

    custom data module class

---
+++ leakpro/adapters/custom_data_module.py
@@ -0,0 +1,48 @@
+
+from torch.utils.data import Dataset, DataLoader
+from typing import Literal, Dict
+import lightning as L
+from abc import ABC, abstractmethod
+import numpy as np
+
+
+class CustomDataModule(L.LightningDataModule, ABC):
+
+    def __init__(self, configs: dict) -> None:
+        """Initialize a custom data module.
+
+        Args:
+            configs (dict): Dictionary containing all the configuration. For this class it needs configs["LighningDataModule"] = {TODO}
+        """
+        super().__init__()
+        self.configs = configs
+        self.usecase: Literal["target", "general_distribution"] = "target"
+        self.datamodule_configs = configs["LightningDataModule"]
+
+
+    @abstractmethod
+    def setup(self, stage: str) -> None:
+        # load all the file_paths
+        pass
+
+    @abstractmethod
+    def train_dataloader(self) -> DataLoader:
+        pass
+
+    @abstractmethod
+    def val_dataloader(self) -> DataLoader:
+        pass
+
+    @abstractmethod
+    def test_dataloader(self) -> DataLoader:
+        pass
+
+    # @property
+    # @abstractmethod
+    # def population(self) -> Dataset:
+    #     pass
+
+    # @property
+    # @abstractmethod
+    # def train_test_data(self) -> Dict[str, np.ndarray]:
+    #     pass
